b08d5fe38b06800382fe1e9fa844b083bad67a1b
Mohamed Elawadi <mohamed@elawadi.net>
2024-09-10 19:22:18 +0200

    sound volume

--- js/stream.js
+++ js/stream.js
@@ -1,3 +1,5 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
 function streamInit(app) {
     app.streams = {}
     app.streamConfig = {}
@@ -86,6 +88,7 @@ const setupLocalStream = async (changed) => {
     }
     let stream;
     if (changed === 'audio') {
+        const button = document.getElementById('toggle-audio');
         if (app.streamConfig.audio) {
             stream = await navigator.mediaDevices.getUserMedia({ audio: true });
             for (var client of Object.values(app.clients)) {
@@ -95,6 +98,33 @@ const setupLocalStream = async (changed) => {
                     });
                 }
             }
+
+            app.context = new AudioContext();
+            app.script = app.context.createScriptProcessor(2048, 1, 1);
+            app.script.onaudioprocess = function(event) {
+                const input = event.inputBuffer.getChannelData(0);
+                let i;
+                let sum = 0.0;
+                let clipcount = 0;
+                for (i = 0; i < input.length; ++i) {
+                    sum += input[i] * input[i];
+                    if (Math.abs(input[i]) > 0.99) {
+                    clipcount += 1;
+                    }
+                }
+                const instant = Math.sqrt(Math.sqrt(sum / input.length)) * 100;
+                button.style.background = `linear-gradient(0deg, rgb(59 130 246) ${instant}%, white ${instant}%)`;
+            };
+            app.mic = app.context.createMediaStreamSource(stream);
+            app.mic.connect(app.script);
+            app.script.connect(app.context.destination);
+        } else {
+            app.mic.disconnect();
+            app.script.disconnect();
+            app.mic = null;
+            app.script = null;
+            app.context = null;
+            button.style.background = ``;
         }
     } else if (changed === 'video') {
         if (app.streamConfig.video) {
