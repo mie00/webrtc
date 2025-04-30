3af3c3857f049033ed239fc08c88ac16bf59a7ab
Mohamed Elawadi <mohamed@elawadi.net>
2025-04-30 05:20:53 +0300

    refactor: Update SDP template to support data channel and simplify structure

--- js/sdpcompress.ts
+++ js/sdpcompress.ts
@@ -8,7 +8,8 @@ interface CompressionLengths {
 }
 
 // The list of SDP patterns
-const list: string = `
+const list: [string] =`
+{str}
 v=0
 o={str} {uint64} {uint8} IN IP4 {ip}
 s=0
@@ -17,130 +18,29 @@ a=group:BUNDLE 0
 a=extmap-allow-mixed
 a=msid-semantic: WMS
 a=msid-semantic:WMS *
-m=video 9 UDP/TLS/RTP/SAVPF {uint8}
-c=IN IP4 0.0.0.0
-a=rtcp:9 IN IP4 0.0.0.0
+m=application {uint16} UDP/DTLS/SCTP webrtc-datachannel
+c=IN IP4 {ip}
+a=candidate:{uint32} 1 udp {uint32} {uuid}.local {uint16} typ host generation 0 network-cost 999
+a=candidate:{uint32} 1 udp {uint32} {ip} {uint16} typ srflx raddr {ip} rport 0 generation 0 network-cost 999
+a=candidate:{uint32} 1 UDP {uint32} {uuid}.local {uint16} typ host
+a=candidate:{uint32} 1 TCP {uint32} {uuid}.local {uint16} typ host tcptype active
+a=candidate:{uint32} 1 UDP {uint32} {ip} {uint16} typ srflx raddr {ip} rport 0
 a=ice-ufrag:{str}
 a=ice-pwd:{str}
 a=ice-options:trickle
-a=fingerprint:sha-256 {str}
-a=setup:{str}
+a=fingerprint:sha-256 {sha256}
+a=setup:actpass
+a=setup:active
+a=setup:passive
 a=mid:0
-a=extmap:1 urn:ietf:params:rtp-hdrext:toffset
-a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
-a=extmap:3 urn:3gpp:video-orientation
-a=extmap:4 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
-a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay
-a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type
-a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing
-a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space
-a=extmap:9 urn:ietf:params:rtp-hdrext:sdes:mid
-a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id
-a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id
-a=recvonly
-a=rtcp-mux
-a=rtcp-rsize
-a=rtpmap:{uint8} VP8/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} VP9/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} profile-id=0
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d001f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=4d001f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} H264/90000
-a=rtcp-fb:{uint8} goog-remb
-a=rtcp-fb:{uint8} transport-cc
-a=rtcp-fb:{uint8} ccm fir
-a=rtcp-fb:{uint8} nack
-a=rtcp-fb:{uint8} nack pli
-a=fmtp:{uint8} level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=f4001f
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=rtpmap:{uint8} ulpfec/90000
-a=rtpmap:{uint8} red/90000
-a=rtpmap:{uint8} rtx/90000
-a=fmtp:{uint8} apt={uint8}
-a=ssrc-group:FID {uint32} {uint32}
-a=ssrc:{uint32} cname:{str}
-a=ssrc:{uint32} msid:{str} {str}
-a=ssrc:{uint32} mslabel:{str}
-a=ssrc:{uint32} label:{str}
-a=ssrc:{uint32} cname:{str}
-a=ssrc:{uint32} msid:{str} {str}
-a=ssrc:{uint32} mslabel:{str}
-a=ssrc:{uint32} label:{str}
-a=candidate:{str} 1 udp {uint32} {ip} {uint16} typ host generation 0 network-cost 999
-a=end-of-candidates
-a=candidate:{str} 1 udp {uint32} {ip} {uint16} typ srflx raddr {ip} rport {uint16} generation 0 network-cost 999
-a=candidate:{str} 1 udp {uint32} {ip} {uint16} typ relay raddr {ip} rport {uint16} generation 0 network-cost 999
-a=candidate:{str} 1 tcp {uint32} {ip} {uint16} typ host tcptype active generation 0 network-cost 999
-a=candidate:{str} 1 tcp {uint32} {ip} {uint16} typ srflx raddr {ip} rport {uint16} tcptype active generation 0 network-cost 999
-a=candidate:{str} 1 tcp {uint32} {ip} {uint16} typ relay raddr {ip} rport {uint16} tcptype active generation 0 network-cost 999
-`;
+a=sctp-port:5000
+a=max-message-size:{uint32}
+a=sendrecv
+s=-
+a=candidate:{uint32} 1 udp {uint32} {ip} {uint16} typ host generation 0 network-id {uint8}
+a=candidate:{uint32} 1 udp {uint32} {ip} {uint16} typ srflx raddr {ip} rport {uint16} generation 0 network-id {uint8}
+a=candidate:{uint32} 1 tcp {uint32} {ip} {uint16} typ host tcptype active generation 0 network-id {uint8}
+`.trim().split('\n');
 
 // Variables used in compression
 const variables: CompressionVariables = {};
