98c45f7790dd2795050da548c8ea4daf0e6e38c5
Mohamed Elawadi <mohamed@elawadi.net>
2025-05-07 23:05:11 +0300

    tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    tailwindcss(),
+  ],
   server: {
     host: '127.0.0.1', // Listen only on localhost IP
     port: 3000
